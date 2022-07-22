3f6d60497face91a84d093b4de7b4b5bbb93db39
Nathan Pope <nathan.pope@bbc.co.uk>
2022-07-22 14:49:32 +0100

    Gave the tests more descriptive names

--- src/test/kotlin/BowlingTest.kt
+++ src/test/kotlin/BowlingTest.kt
@@ -6,8 +6,7 @@ class BowlingTest {
     private val bowling = Bowling()
 
     @Test
-    fun `12 strikes`() {
-//        X X X X X X X X X X X X (12 rolls: 12 strikes) = 10 frames * 30 points = 300
+    fun `a sequence of 12 strikes has a game score of 300`() {
         val result = bowling.game(
             turns = listOf(
                 listOf('X'),
@@ -22,13 +21,11 @@ class BowlingTest {
                 listOf('X', 'X', 'X')
             )
         )
-
         assertEquals(expected = 300, actual = result)
     }
 
     @Test
-    fun `10 pairs of 9 and miss`() {
-        // 9- 9- 9- 9- 9- 9- 9- 9- 9- 9- (20 rolls: 10 pairs of 9 and miss) = 10 frames * 9 points = 90
+    fun `a sequence of 10 pairs of 9 and miss has a game score of 90`() {
         val result = bowling.game(
             turns = listOf(
                 listOf(9, '-'),
@@ -43,13 +40,11 @@ class BowlingTest {
                 listOf(9, '-')
             )
         )
-
         assertEquals(expected = 90, actual = result)
     }
 
     @Test
-    fun `10 pairs of 5 and spare, with a final 5`() {
-        // 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5 (21 rolls: 10 pairs of 5 and spare, with a final 5) = 10 frames * 15 points = 150
+    fun `a sequence of 10 pairs of 5 and spare, with a final 5 has a game score of 150`() {
         val result = bowling.game(
             turns = listOf(
                 listOf(5, '/'),
